74766eaf19b0ac2992e01c7f304f393688348805
Gerry Sousa <gerry@ciandt.com>
2021-07-09 16:08:03 -0300

    Curso Java Completo - Aula 07: Operador resto e operadores de comparação

--- JavaOnline/src/Operadores.java
+++ JavaOnline/src/Operadores.java
@@ -15,5 +15,13 @@ public class Operadores {
     System.out.println("A multiplicacao e: "+(numero1*numero2));//A multiplicacao é: 200
     System.out.println("A divisao e: "+(numero1/numero2));//A divisao é: 0   -- como estamos usando inteiros, 0.5 , depois da virgula é ignorado
 
+    System.out.println("Se resto da divisao por 2 eh = 0 (numero é par), se resto é =1 (numero eh impar)");
+    System.out.println("Resto da divisao eh: "+(21%2));
+    System.out.println("Resto da divisao eh: "+(20%2));
+
+    System.out.println("Fazendo cast de variavel");
+    double numDouble = 10d;
+    float numFloat = (float) numDouble;// a conversão direto assim (float numFloat = numDouble;) retorna erro
+    System.out.println("numFloat convertido eh:"+numFloat);
   }
 }
