54ce04e1cf0140032d92a0ab628e563907daf812
Gerry Sousa <gerry@ciandt.com>
2021-07-09 08:21:36 -0300

    Curso Java Completo - Aula 05: Tipos primitivos, convenções de código e comentários

---
+++ JavaOnline/src/ImprimindoVariaveis.java
@@ -0,0 +1,25 @@
+public class ImprimindoVariaveis {
+
+  public static void main(String[] args) {
+    //comentario unica linha
+    /*
+    Comentario
+    varias
+    linhas
+     */
+    /**
+     * Comentario oficial java
+     * Tipos primitivos
+     */
+
+      int idade = 20;
+      double preco = 10.5;
+      float valor = 10;
+      boolean existe = true;
+      char letra = 'A';//aspas simples
+      byte tests = 1;
+      short test = 1;
+      long numeroGrand = 10000000;
+    System.out.println("A idade eh: "+idade);
+  }
+}
