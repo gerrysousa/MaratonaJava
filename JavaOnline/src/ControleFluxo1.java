5282383717c6dd51122d846dfac12ad477909ee2
Gerry Sousa <gerry@ciandt.com>
2021-07-09 17:15:01 -0300

    Curso Java Completo - Aula 10: Controle de fluxo if, else if e else

---
+++ JavaOnline/src/ControleFluxo1.java
@@ -0,0 +1,21 @@
+public class ControleFluxo1 {
+
+  public static void main(String[] args) {
+    //idade < 15 categoria infantil
+    //idade >= 15 && <18 categoria juvenil
+    //idade > 18 categoria adulto
+
+    int idade = 18;
+    String categoria;
+
+    if (idade<15){
+      categoria="categoria infantil";
+    }else if (idade>=15 && idade <18){
+      categoria="categoria juvenil";
+    }else {
+      categoria="categoria adulto";
+    }
+    System.out.println("Pertence a: "+categoria);
+
+  }
+}
