3b0c6f7e9394b8b099e15a4fb312c7eb83104411
Gerry Sousa <gerry@ciandt.com>
2021-07-09 15:57:39 -0300

    Curso Java Completo - Aula 07: Operador resto e operadores de comparação

---
+++ JavaOnline/src/OperadoresDeComparacao.java
@@ -0,0 +1,16 @@
+public class OperadoresDeComparacao {
+
+  public static void main(String[] args) {
+    boolean dezMaiorQueVinte = 10 >=20;
+    System.out.println("Variavel dezMaiorQueVinte= "+dezMaiorQueVinte);
+    boolean dezMenorIgualVinte = 10 <=20;
+    System.out.println("Variavel dezMenorIgualVinte= "+dezMenorIgualVinte);
+
+    System.out.println("Comparador 'igual' (5==5):"+(5==5));
+    System.out.println("Comparador 'diferente' (5!=5):"+(5!=5));
+    System.out.println("Comparador 'maior' (5>5):"+(5>5));
+    System.out.println("Comparador 'menor' (5<5):"+(5<5));
+    System.out.println("Comparador 'maior igual' (5>=5):"+(5>=5));
+    System.out.println("Comparador 'menor igual' (5<=5):"+(5<=5));
+  }
+}
