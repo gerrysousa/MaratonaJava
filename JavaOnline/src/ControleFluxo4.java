17ef962dab524b299e44d25baeeb4f3d929ae3bb
Gerry Sousa <gerry@ciandt.com>
2021-07-10 10:49:05 -0300

    Curso Java Completo - Aula 15: Controle de fluxo for, while, do while

---
+++ JavaOnline/src/ControleFluxo4.java
@@ -0,0 +1,19 @@
+public class ControleFluxo4 {
+
+  public static void main(String[] args) {
+    int contador=0, contator2=12;
+    while (contador <10){
+      System.out.println(++contador);
+      //contador++;
+    }
+    do {
+      System.out.println(contator2);
+      contator2++;
+    }while (contator2<10);
+
+    for (int i=0;i<10;i++){
+      System.out.println("Valor de i eh: "+i);
+    }
+
+  }
+}
