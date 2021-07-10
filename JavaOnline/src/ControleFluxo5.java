567515ccc4e6158b8c98a5d092194e2a9ce7fdd0
Gerry Sousa <gerry@ciandt.com>
2021-07-10 13:36:35 -0300

    Curso Java Completo - Aula 17: Comando break and continue

---
+++ JavaOnline/src/ControleFluxo5.java
@@ -0,0 +1,17 @@
+public class ControleFluxo5 {
+
+  public static void main(String[] args) {
+    //Dado o valor de um carro, descubra quantas vezes ele pode ser parcelado
+    //Porem as parcelas não podem ser menores que 1000
+
+    double valorTotal = 30000;
+
+    for (int parcela = 1; parcela <=valorTotal; parcela++){
+      double valorParcela = valorTotal /parcela;
+      if (valorParcela<1000){
+        break;
+      }
+      System.out.println("Pode ser dividido em "+parcela+" parcelas de R$ "+valorParcela);
+    }
+  }
+}
---
+++ JavaOnline/src/ControleFluxo6.java
@@ -0,0 +1,17 @@
+public class ControleFluxo6 {
+  public static void main(String[] args) {
+    //Dado o valor de um carro, descubra quantas vezes ele pode ser parcelado
+    //Porem as parcelas não podem ser menores que 1000
+
+    double valorTotal = 30000;
+
+    for (int parcela = (int)valorTotal; parcela >=1; parcela--){
+      double valorParcela = valorTotal /parcela;
+      if (valorParcela<=1000){
+        //o continue vai voltar ao inicio do for, e ignorar o codigo abaixo dele
+        continue;
+      }
+      System.out.println("Pode ser dividido em "+parcela+" parcelas de R$ "+valorParcela);
+    }
+  }
+}
