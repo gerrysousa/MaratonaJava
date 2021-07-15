7b6d56e4fe149caa38d572c8b952a8a6aefbf855
Gerry Sousa <gerry@ciandt.com>
2021-07-15 06:35:11 -0300

    Curso Java Completo - Aula 51: Herança parte 2 e referência super

--- JavaOnline/src/estudo/java/javacore/_8heranca/classes/Funcionario.java
+++ JavaOnline/src/estudo/java/javacore/_8heranca/classes/Funcionario.java
@@ -3,6 +3,12 @@ package estudo.java.javacore._8heranca.classes;
 public class Funcionario  extends Pessoa{
  private double salario;
 
+
+  public void print() {
+    super.print();
+    System.out.println("Salario: "+this.salario);
+  }
+
   public double getSalario() {
     return salario;
   }
