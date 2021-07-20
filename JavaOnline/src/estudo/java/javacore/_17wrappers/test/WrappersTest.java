0db526f5cd192f8cf79c1ec3a89637a610db4c48
Gerry Sousa <gerry@ciandt.com>
2021-07-20 07:18:27 -0300

    Curso Java Completo - Aula 79: Classes Wrapper pt 02

--- JavaOnline/src/estudo/java/javacore/_17wrappers/test/WrappersTest.java
+++ JavaOnline/src/estudo/java/javacore/_17wrappers/test/WrappersTest.java
@@ -14,12 +14,30 @@ public class WrappersTest {
 
     Byte byteWrapper = 1;
     Short shortWrapper = 1;
-    Integer integerWrapper = 10;
-    Long longWrapper = 10L;
+    Integer integerWrapper = 10; //pode ser inicializado assim: Integer integerWrapper = new Integer('10'); aceita string
+    Long longWrapper = 10L; //pode ser inicializado assim: Long longWrapper = Long.valueOf("10");
+
     Float floatWrapper = 10F;
     Double doubleWrapper = 10D;
-    Character characterWrapper = 'A';
-    Boolean booleanWrapper = true;
+    Character characterWrapper = 'A';// não aceita string: Character characterWrapper = new Character('A');
+    Boolean booleanWrapper = true;//case insensitive, aceita maiusculo ou minusclo : Boolean booleanWrapper = new Boolean("True");
+
+
+    int iUnbox = integerWrapper; // Isso é chamado de unboxing
+    System.out.println("Unboxing: "+iUnbox);
+
+    String valorString = "10";
+    Float f = Float.parseFloat(valorString);
+    System.out.println(f);
+
+    System.out.println("-------------------- metodos uteis --------------------");
+    System.out.println(Character.isDigit('A'));
+    System.out.println(Character.isLetter('9'));
+    System.out.println(Character.isLetterOrDigit('#'));
+    System.out.println(Character.isUpperCase('a'));
+    System.out.println(Character.isLowerCase('b'));
+    System.out.println(Character.toUpperCase('a'));
+
 
   }
 }
