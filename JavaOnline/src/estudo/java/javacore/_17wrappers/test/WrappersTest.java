dcb91e11e56f773b509eca6e0e68831e6c6b20ce
Gerry Sousa <gerry@ciandt.com>
2021-07-20 06:53:10 -0300

    Curso Java Completo - Aula 78: Classes Wrapper pt 01

---
+++ JavaOnline/src/estudo/java/javacore/_17wrappers/test/WrappersTest.java
@@ -0,0 +1,25 @@
+package estudo.java.javacore._17wrappers.test;
+
+public class WrappersTest {
+
+  public static void main(String[] args) {
+    byte bytePrimitivo = 1;
+    short shortPrimitivo = 1;
+    int intPrimitivo = 10;
+    long longPrimitivo = 10;
+    float floatPrimitivo = 10;
+    double doublePrimitivo = 10;
+    char charPrimitivo = 'A';
+    boolean booleanPrimitivo = true;
+
+    Byte byteWrapper = 1;
+    Short shortWrapper = 1;
+    Integer integerWrapper = 10;
+    Long longWrapper = 10L;
+    Float floatWrapper = 10F;
+    Double doubleWrapper = 10D;
+    Character characterWrapper = 'A';
+    Boolean booleanWrapper = true;
+
+  }
+}
