c188f792580063ffe60d331c153c7812c1f25386
Gerry Sousa <gerry@ciandt.com>
2021-07-30 07:22:33 -0300

    Curso Java Completo - Aula 110: NIO pt 08 PosixFileAttributes e PosixFileAttributeView

---
+++ JavaOnline/src/estudo/java/javacore/_23nio/test/PosixFileAttributesTest.java
@@ -0,0 +1,25 @@
+package estudo.java.javacore._23nio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
+public class PosixFileAttributesTest {
+  public static void main(String[] args) throws IOException {
+    System.out.println("Iniciando >>> ");
+    Path path = Paths.get("pasta/arquivo.txt");
+    PosixFileAttributes posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+    System.out.println("Permissions: "+posixFileAttributes.permissions());
+    System.out.println("Alterando permissões ---------");
+    Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rw-rw-rw-");
+    Files.setPosixFilePermissions(path, permissions);
+    posixFileAttributes = Files.readAttributes(path, PosixFileAttributes.class);
+    System.out.println("Permissions: "+posixFileAttributes.permissions());
+  }
+
+}
