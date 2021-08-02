fb0b08f5e1058fe6d2b788102e6f6138ada60cab
Gerry Sousa <gerry@ciandt.com>
2021-08-02 07:48:35 -0300

    Curso Java Completo - Aula 117: Coleções pt 02 método hashCode

--- JavaOnline/src/estudo/java/javacore/_25colecoes/classes/Celular.java
+++ JavaOnline/src/estudo/java/javacore/_25colecoes/classes/Celular.java
@@ -1,5 +1,7 @@
 package estudo.java.javacore._25colecoes.classes;
 
+import javax.swing.ImageIcon;
+
 public class Celular {
 
   private String nome;
@@ -9,6 +11,21 @@ public class Celular {
     this.nome = nome;
     this.IMEI = IMEI;
   }
+  /***
+   * toString()
+   Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+   Simetrico: para x e y diferentes de null, se x.equals(y) == true, logo y.equals(x) tem que ser true
+   Transitivo: para x, y, z diferentes de null, se x.equals(y)== true, logo y.equals(x) == true e x.equals(z) == true, logo y.equals(z) também tem que ser true
+   Constante: x.equals(y) deve sempre retornar o mesmo valor, para x diferente de null x.equals(null) tem que retornar false
+   */
+  /***
+   * hashCode()
+   Se x.equals(y) == true y.hashCode() == x.hashCode();
+   y.hashCode() == x.hashCode() não necessariamente o equals deverá retornar true
+   Se x.equals(y) == false, então não precisa ter nada no hashCode, pois retornou false
+   Se y.hashCode() != x.hashCode() , x.equals(y) deverá ser false
+    */
+
 
   @Override
   public boolean equals(Object obj) {
@@ -25,13 +42,10 @@ public class Celular {
     return IMEI != null && IMEI.equals(outroCelular.IMEI);
   }
 
-  /***
-   Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
-   Simetrico: para x e y diferentes de null, se x.equals(y) == true, logo y.equals(x) tem que ser true
-   Transitivo: para x, y, z diferentes de null, se x.equals(y)== true, logo y.equals(x) == true e x.equals(z) == true, logo y.equals(z) também tem que ser true
-   Constante: x.equals(y) deve sempre retornar o mesmo valor, para x diferente de null x.equals(null) tem que retornar false
-   */
-
+  @Override
+  public int hashCode() {
+    return IMEI != null ? IMEI.hashCode() : 1;
+  }
 
   public String getNome() {
     return nome;
