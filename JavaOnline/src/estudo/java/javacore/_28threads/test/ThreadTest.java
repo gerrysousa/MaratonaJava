34b754cbdf8434be2777c6339855d9fd2f9702fb
Gerry Sousa <gerry@ciandt.com>
2021-11-24 08:48:42 -0300

    Curso Java Completo - Aula 158: Threads pt 03 Parando a execução com Sleep, Yield e Join

--- JavaOnline/src/estudo/java/javacore/_28threads/test/ThreadTest.java
+++ JavaOnline/src/estudo/java/javacore/_28threads/test/ThreadTest.java
@@ -46,16 +46,31 @@ class ThreadExemploRunnable implements Runnable {
       if (i % 100 == 0) {
         System.out.println();
       }
+      Thread.yield();
+//      try {
+//        Thread.sleep(100);
+//      } catch (InterruptedException e) {
+//        e.printStackTrace();
+//      }
     }
   }
 }
 
 public class ThreadTest {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     /**
-     Se vc executar a Thread com run, vai executar somente uma Thread por vez, op correto é utilizar o comando "start"
+     - Se vc executar a Thread com run, vai executar somente uma Thread por vez, op correto é utilizar o comando "start"
      t1.run();    t2.run();    t3.run();    t4.run();
+     - Unica forma de fazer uma Thread parar a execução é o Sleep,
+     - Prioridade das Threads: MAX_PRIORITY(10), NORM_PRIORITY(5), MIN_PRIORITY(1)
+     - Thread.yield(): Ele faz a Thread que está executando voltar para o status Runnable, e dá uma chance para outras Threads executarem
+     - td2.join(): o Join força a uma "thread A(Pai)" que se juntou esperar a conclusão da Thread B para seguir as linhas abaixo,
+        no caso a "thread main" se junta a "thread T2", enquando a "thread T2" não terminar a "thread T3" não será iniciada no codigo abaixo
+         td1.start();
+         td2.start();
+         td2.join();// nesse ponto as Threads T1 e T2 ficam se alternando
+         td3.start();
      */
 //    ThreadExemplo t1 = new ThreadExemplo('A');
 //    ThreadExemplo t2 = new ThreadExemplo('B');
@@ -71,11 +86,15 @@ public class ThreadTest {
     Thread td2 = new Thread(new ThreadExemploRunnable('B'));
     Thread td3 = new Thread(new ThreadExemploRunnable('C'));
     Thread td4 = new Thread(new ThreadExemploRunnable('D'));
+    td4.setPriority(Thread.MAX_PRIORITY);
 
     td1.start();
     td2.start();
+    td2.join();
+
     td3.start();
     td4.start();
 
   }
+
 }
