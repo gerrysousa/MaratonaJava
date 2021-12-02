a478dab07c4514f17f853c278b3c6a15cee2cf19
Gerry Sousa <gerry@ciandt.com>
2021-12-02 07:34:22 -0300

    Curso Java Completo - Aula 162: Concorrência pt 01: Variáveis atômicas

---
+++ JavaOnline/src/estudo/java/javacore/_29concorrencia/test/ConcorrenciaTest.java
@@ -0,0 +1,56 @@
+package estudo.java.javacore._29concorrencia.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Contador {
+
+  private int count;
+  private AtomicInteger atomicInteger = new AtomicInteger();
+
+  public void increment() {
+    /***
+    - se colocar o metodo como "synchronized"  o count vai funcionar corretamente e retornar 20000,
+    - porém "synchronized" demanda mais do processador
+     */
+    count++;
+    atomicInteger.getAndIncrement();
+  }
+
+  public int getCount() {
+    return count;
+  }
+  public int getAtomicInteger(){
+    return atomicInteger.intValue();
+  }
+}
+
+class IncrementadorThread extends Thread {
+
+  private Contador contador;
+
+  public IncrementadorThread(Contador contador) {
+    this.contador = contador;
+  }
+
+  public void run() {
+    for (int i = 0; i < 10000; i++) {
+      contador.increment();
+    }
+  }
+}
+
+public class ConcorrenciaTest {
+
+  public static void main(String[] args) throws InterruptedException {
+    Contador c = new Contador();
+    IncrementadorThread it1 = new IncrementadorThread(c);
+    IncrementadorThread it2 = new IncrementadorThread(c);
+    it1.start();
+    it2.start();
+    it1.join();
+    it2.join();
+    System.out.println(c.getCount());
+    System.out.println(c.getAtomicInteger());
+  }
+
+}
